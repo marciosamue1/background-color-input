7fbc192dcf9048d10ec857507d273756dc8442ad
yogrumm1234 <marciosamuel140@gmail.com>
2026-03-01 17:54:44 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,36 +1,25 @@
-function escopo() {
+function escopo () {
+    
+    
+    function recebeEvento (evento) { //Evento de submit
+    
+    evento.preventDefault();
+        
 
-
-    function recebeEvento(evento) { //Evento de submit
-
-        evento.preventDefault();
-
-
-
-        campoDeMudanca.style.background = valor.value; //Aqui estou adicionando a cor, que está no input
-
-
-        //Aqui eu estou adicionando o nome da cor e já estou adicionando uma cor para o elemento
-        corAtual.innerHTML = valor.value;
-        corAtual.style.color = valor.value;
-
-        if (valor.value === "black") {
-            corAtual.style.color = 'white';
-        }
+    
+    campoDeMudanca.style.background =  valor.value; //Aqui estou adicionando a cor, que está no input
+    
+    
+    //Aqui eu estou adicionando o nome da cor e já estou adicionando uma cor para o elemento
+    corAtual.innerHTML = valor.value;
+    corAtual.style.color = valor.value;
+    
+    if(valor.value === "black") {
+        corAtual.style.color = 'white';
     }
-    const form = document.querySelector('#form');
-
-    function recebeEvento(evento) {
-
-        evento.preventDefault();
-
-        const campoDeMudanca = document.querySelector('#container');
-        const valor = document.querySelector('.texto')
-
-        campoDeMudanca.style.background = valor.value
-    }
-
-
+}
+    
+    
     form.addEventListener('submit', recebeEvento);
 
 }
@@ -43,5 +32,4 @@ const form = document.querySelector('#form'); // selecionei o form
 
 const corAtual = document.querySelector('.SuaCorAtual');
 
-
-escopo()
+escopo ()
